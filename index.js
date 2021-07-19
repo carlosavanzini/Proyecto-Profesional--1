bd84285e5d59a6ce377ca13dbad9db28e8eb5e01
Alberto <albertodamianlopez@gmail.com>
2021-07-19 00:29:24 -0300

    Modificación de errores gramaticales en archivos index.js contacto.js y productos.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let nombre = "";
 let email = "";
 let confirmacion = "";
@@ -7,79 +6,79 @@ const fecha = new Date();
 const hora = fecha.getHours();
 
 
-if (localStorage.getItem("confirmacion")==null){
-  
-  confirmacion = confirm("Desea ingresar Nombre e email?")
-  localStorage.setItem("confirmacion", confirmacion)
-  if(confirmacion==true){
-    inicioUsuario()
-    
-  }
+if (localStorage.getItem("confirmacion") == null) {
+
+    confirmacion = confirm("Desea ingresar nombre y email?")
+    localStorage.setItem("confirmacion", confirmacion)
+    if (confirmacion == true) {
+        inicioUsuario()
+
+    }
 }
 
-  
-  //Funciones
 
-  //Inicio de usuario
-  function inicioUsuario(){
+//Funciones
+
+//Inicio de usuario
+function inicioUsuario() {
     nombreValid()
     emailValid()
-    
-  }
-  //Guardar y validar nombre
-  function nombreValid(nombre){
-      do {
-          nombre = prompt("Ingrese su nombre")
-          
-          if(nombre.trim() == ""){
-              alert("Ingrese datos validos")
-          }else{return localStorage.setItem("nombre", nombre) }
-      } while (nombre=="" || nombre!==undefined);
-  }
-  //Guardar y validar email
-  function guardarEmail(email) {
+
+}
+//Guardar y validar nombre
+function nombreValid(nombre) {
+    do {
+        nombre = prompt("Ingrese su nombre")
+
+        if (nombre.trim() == "") {
+            alert("Ingrese datos válidos")
+        } else { return localStorage.setItem("nombre", nombre) }
+    } while (nombre == "" || nombre !== undefined);
+}
+//Guardar y validar email
+function guardarEmail(email) {
     email = prompt("Ingrese su email");
     let arrEmail = email.split("");
     if (arrEmail.includes("@") && arrEmail.includes(".")) {
-      localStorage.setItem("email", email);
-      enviarMail(mailTo);
+        localStorage.setItem("email", email);
+        enviarMail(mailTo);
     } else {
-      alert("Datos invalidos");
+        alert("Datos invalidos");
     }
-  }
-  
-  function emailValid(email) {
-    do {
-      guardarEmail(email);
-    } while (localStorage.getItem("email") === null);
-  }
+}
 
-  //Preguntar para enviar mail
-  function enviarMail(mail) {
+function emailValid(email) {
+    do {
+        guardarEmail(email);
+    } while (localStorage.getItem("email") === null);
+}
+
+//Preguntar para enviar mail
+function enviarMail(mail) {
     mail = confirm("Desea recibir mails con novedades?");
     if (mail == true) {
-      alert(
-        "Estaremos enviandole las ultimas novedades a " +
-          localStorage.getItem("email")
-      );
-      localStorage.setItem("mail", mail);
-      
+        alert(
+            "Estaremos enviandole las últimas novedades a " +
+            localStorage.getItem("email")
+        );
+        localStorage.setItem("mail", mail);
+
     }
-  }
+}
 
-  function saludaHoraInicio() {
+function saludaHoraInicio() {
     if (hora >= 00 && hora <= 06) {
-      alert("Buenas madrugadas " + localStorage.getItem("nombre") + ". Difrute la pagina!!!" );
-  }
-  if (hora >= 07 && hora <= 12) {
-    alert("Buen dia " + localStorage.getItem("nombre") + ". Difrute la pagina!!!");
-  }
-  if (hora >= 13 && hora <= 18) {
-    alert("Buenas tardes " + localStorage.getItem("nombre") + ". Difrute la pagina!!!");
-  }
-  if (hora >= 19 && hora <= 23) {
-    alert("Buenas noches " + localStorage.getItem("nombre") + ". Difrute la pagina!!!");
-  }
-  }
+        alert("Buenas madrugadas " + localStorage.getItem("nombre") + ". Disfrute la página!!!");
+    }
+    if (hora >= 07 && hora <= 12) {
+        alert("Buen día " + localStorage.getItem("nombre") + ". Disfrute la página!!!");
+    }
+    if (hora >= 13 && hora <= 18) {
+        alert("Buenas tardes " + localStorage.getItem("nombre") + ". Disfrute la página!!!");
+    }
+    if (hora >= 19 && hora <= 23) {
+        alert("Buenas noches " + localStorage.getItem("nombre") + ". Disfrute la página!!!");
+    }
+}
 
-  saludaHoraInicio()
+saludaHoraInicio()
