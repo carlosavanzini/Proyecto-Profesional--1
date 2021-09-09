ee59514029debe3bc46dd4eed4d31fcbb67bae8f
Alberto <albertodamianlopez@gmail.com>
2021-09-09 17:36:15 -0300

    Envío de datos del usuario

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ let mailTo;
 const fecha = new Date();
 const hora = fecha.getHours();
 
+// Declaramos variables globales para armar el objeto.
+let tokenObjeto = '';
+let nombreObjeto = "";
+let emailObjeto = "";
+let confirmacionMailObjeto = Boolean;
 
 if (localStorage.getItem("confirmacion") == null) {
 
@@ -32,7 +37,10 @@ function nombreValid(nombre) {
 
         if (nombre.trim() == "") {
             alert("Ha ingresado un nombre inexistente. Por favor ingrese un nombre")
-        } else { return localStorage.setItem("nombre", nombre) }
+        } else {
+            nombreObjeto = nombre;
+            return localStorage.setItem("nombre", nombre)
+        }
     } while (nombre == "" || nombre !== undefined);
 }
 //Guardar y validar email
@@ -41,6 +49,7 @@ function guardarEmail(email) {
     let arrEmail = email.split("");
     if (arrEmail.includes("@") && arrEmail.includes(".")) {
         localStorage.setItem("email", email);
+        emailObjeto = email;
         enviarMail(mailTo);
     } else {
         alert("El mail ingresado es inexistente. El mail debe contar al menos con '@' y '.'");
@@ -56,14 +65,78 @@ function emailValid(email) {
 //Preguntar para enviar mail
 function enviarMail(mail) {
     mail = confirm("Desea recibir mails con novedades?");
+    conformacionMail = mail;
     if (mail == true) {
         alert(
             "Estaremos enviandole las últimas novedades a " +
             localStorage.getItem("email")
         );
         localStorage.setItem("mail", mail);
-
     }
+    confirmacionMailObjeto = mail;
+    console.log(confirmacionMailObjeto);
+}
+
+
+///////////// Enviamos los datos del usuario. ///////////////////
+
+// Si no se completó con info en el local storage, no enviamos nada al servidor
+if (nombreObjeto === "" || emailObjeto === "" || confirmacion == false) {
+
+    console.log("No se van a enviar los datos del usuario al servidor");
+
+} else {
+
+    // Vamos a armar un token aleatorio con longitud 10.
+    const armarToken = longitudDiez => {
+        let cadena = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        // Obtengo la longitud total de la cadena (62)
+        let cadenaLength = cadena.length;
+        for (let i = 0; i < longitudDiez; i++) {
+            // Cada caracter posee un random por la longitud de la cadena
+            tokenObjeto += cadena.charAt(Math.floor(Math.random() * cadenaLength));
+        }
+        return tokenObjeto;
+    }
+
+    console.log(armarToken(10));
+
+    // Guardamos los datos en un objeto. 
+    let datos = {
+        token: tokenObjeto,
+        name: nombreObjeto,
+        email: emailObjeto,
+        sendEmail: confirmacionMailObjeto
+    };
+
+    console.log(datos);
+
+    fetch("https://formsubmit.co/ajax/albertodamianlopez@gmail.com", {
+        //fetch("https://demo2420474.mockable.io/submitForm", {
+        method: "POST",
+        headers: { // Cabeceras HTTP
+            // Ennviamos los datos en formato JSON
+            'Content-Type': 'application/json'
+        },
+        // El cuerpo de la petición es una cadena de texto con los datos en formato JSON
+        body: JSON.stringify(datos)
+    })
+
+    .then(res => res.ok ? res.json() : Promise.reject(res))
+        // Si esta todo OK, muestro mensaje por pantalla que todo se realizó correctamente.
+        .then(data => {
+            console.log(data);
+            console.log("Sus datos de usuario fueron enviados con éxito");
+        })
+        // En caso de error, devuelvemos un error personalizado
+        .catch(err => {
+            console.log(err);
+        })
+        // Contamos 1 segundo y finalizamos la petición.
+        .finally(() => setTimeout(() => {
+            console.log("Fin de la petición de usuarios");
+        }, 1000));
+
 }
 
 // Selector de la imagen banner
@@ -84,5 +157,5 @@ fetch(urlBanner)
     })
     // Contamos 1 segundo y finalizamos la petición.
     .finally(() => setTimeout(() => {
-        console.log("Fin de la petición");
+        console.log("Fin de la petición de la imagen banner");
     }, 1000));
